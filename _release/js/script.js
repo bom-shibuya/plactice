aa040d882cd428771c67fc53893dd93f546a26c5
iMac26 <iMac26@iMac26-no-iMac.local>
2016-09-16 14:00:23 +0900

    add 980fixed src/index.html change

--- _release/js/script.js
+++ _release/js/script.js
@@ -1,6 +1,16 @@
-/*! bundled at:2016-08-29 121458 */
+/*! bundled at:2016-09-06 113328 */
 
-// jquery ??????????
+// jquery ƒ‰ƒCƒuƒ‰ƒŠ‚Æ‚©
 
 
 // ‚±‚±‚ÅÀs‚ğ‘‚¢‚Ä‚¢‚­
+$(function(){
+  // smoothscroll
+  $('.js-smooth').on('click',function(e){
+    e.preventDefault();
+    var _targetTop = $($(this).attr('href')).get(0).offsetTop;
+    $('html,body').animate({
+      scrollTop : _targetTop
+    }, 'normal');
+  });
+});
